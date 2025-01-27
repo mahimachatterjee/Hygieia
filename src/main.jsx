a3e82dd048bcb5d16da900f3776f1b5704c59487
Mahima Chatterjee <95462164+mahimachatterjee@users.noreply.github.com>
2025-01-27 16:03:42 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import "/Diet.css" ;
+import "/Exercise.css";
+import "/Vitals.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
